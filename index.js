4160f68c27ff8b5455be22d5ef9ead636e4dcca9
Nasam Sayour <nasamsay@gmail.com>
2022-08-29 16:32:56 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+//for loop
+
+function writeCards(arr, event){
+    const newArr = []
+    for(i=0; i<arr.length; i++){
+        newArr.push(`Thank you ${arr[i]}, for the wonderful ${event} gift.`)
+    }
+    return newArr
+}
+
+
+//while loop
+
+function countdown(int){
+    let i =  int
+    while(i>= 0){
+        console.log(i)
+        i--
+    }
+}
